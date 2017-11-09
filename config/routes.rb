c1abaa248f57374337c67f47385a29b9fe4c14fd
Daan Rosbergen <d.rosbergen@gmail.com>
2017-11-09 18:22:03 +0100

    Add json as default response

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :advertisments
-  post 'auth/login' => 'user_token#create'
-  get 'users/me' => 'users#me'
-  resources :users, :company
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  defaults format: :json do  
+    resources :advertisments
+    post 'auth/login' => 'user_token#create'
+    get 'users/me' => 'users#me'
+    resources :users, :company
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  end
 end
