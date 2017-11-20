b4929b9534d0d748fd2ef8c66247d5ca33358f16
Daan Rosbergen <d.rosbergen@gmail.com>
2017-11-20 20:36:24 +0100

    Add fake data on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
 
-companies = Company.create([{ name: 'Sprout', address: 'Enterij 58', zipcode: '2771MX', country: 'Nederland', city: 'Boskoop' }])
+Faker::Config.locale = 'nl'
+
+companies = [{ name: 'Sprout', address: 'Enterij 58', zipcode: '2771MX', country: 'Nederland', city: 'Boskoop' }]
+49.times do |index|
+    companies.push({
+        name: Faker::Company.name,
+        address: Faker::Address.street_address,
+        zipcode: Faker::Address.zip_code,
+        country: 'Nederland',
+        city: Faker::Address.city
+    })
+end
+companies = Company.create!(companies)
 puts "Created #{companies.count} companies"
 
-users = User.create([{email: 'd.rosbergen@gmail.com', password: 'Welkom#1', company: companies.first}])
+users = [{email: 'd.rosbergen@gmail.com', password: 'Welkom#1', company: companies.first}]
+99.times do |index|
+    users.push({
+        email: "d.rosbergen+#{Faker::Internet.unique.user_name}@gmail.com",
+        password: "Welkom#1",
+        company: companies.sample
+    })
+end
+users = User.create!(users)
 puts "Created #{users.count} users"
